432e42ac995be328e4711659286ba162ef226ff8
박용은 <jeiel85@gmail.com>
2026-04-20 13:44:50 +0900

    refactor: Isar를 Shared Preferences로 대체하여 빌드 안정성 확보

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -29,34 +29,6 @@ subprojects {
             android.defaultConfig.targetSdkVersion(36)
         }
     }
-
-    // 1. AndroidManifest.xml에서 package 속성 제거 (AGP 8.0+ 대응)
-    tasks.withType<com.android.build.gradle.tasks.GenerateResValues>().configureEach {
-        val manifestFile = file("src/main/AndroidManifest.xml")
-        if (manifestFile.exists()) {
-            val content = manifestFile.readText()
-            if (content.contains("package=")) {
-                val updatedContent = content.replace(Regex("package=\"[^\"]*\""), "")
-                manifestFile.writeText(updatedContent)
-            }
-        }
-    }
-
-    // 2. lStar 리소스 오류 강제 해결 (mergeResources 태스크 이후 실행)
-    project.tasks.whenTaskAdded {
-        if (name.startsWith("merge") && name.endsWith("Resources")) {
-            doLast {
-                val valuesFiles = fileTree(mapOf("dir" to "build/intermediates/merged_res", "include" to "**/values.xml"))
-                valuesFiles.forEach { file ->
-                    val content = file.readText()
-                    if (content.contains("android:attr/lStar")) {
-                        val updatedContent = content.replace(Regex("<item name=\"android:attr/lStar\">.*?</item>"), "")
-                        file.writeText(updatedContent)
-                    }
-                }
-            }
-        }
-    }
 }
 
 tasks.register<Delete>("clean") {
